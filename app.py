63e6bf6d1dc4b6ffcf6f8926ad8ba22545617a55
Sanskaragrawal2107 <sanskar21072005@gmail.com>
2025-04-12 12:27:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ def whatsapp_webhook():
     reply = MessagingResponse()
     reply.message(str(response))
     return str(reply)
-
 if __name__ == "__main__":
-   import os
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
